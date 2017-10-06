5eca576595dda3e1af1f807e46f5e3d692622325
nickoliasxii <nickoliasxii@gmail.com>
2017-10-06 17:37:39 -0500

    finished, brah

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,68 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
- "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var guessesLeft = 9;
+var guesses = 12;
+var guessesLeft = 12;
 var letterToGuess = null;
 var guessedLetters = [];
-  
 
 
 
 
-document.onkeyup = function(event) {
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+var updateGuessesLeft = function () {
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function () {
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function () {
+
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+// Reset for after 12 guesses
+var reset = function () {
+    totalGuesses = 12;
+    guessesLeft = 12;
+    guessedLetters = [];
+
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+// WHERE THE MAGIC HAPPENS
+
+document.onkeyup = function (event) {
     guessesLeft--;
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+    var userGuess = String.fromCharCode(event.keyCode);
     guessedLetters.push(userGuess);
     updateGuessesLeft();
     updateGuessesSoFar();
 
     if (guessesLeft > 0) {
-        if (userGuess == letterToGuess) {
+        if (userGuess === letterToGuess) {
             wins++;
             document.querySelector('#wins').innerHTML = "Wins: " + wins;
             alert("Yes, you are psychic!");
             reset();
         }
-    } else if (guessesLeft == 0) {
-        // Then we will loss and we'll update the html to display the loss 
+    } else if (guessesLeft === 0) {
         losses++;
         document.querySelector('#losses').innerHTML = "Losses: " + losses;
-        alert("Sorry, you're not psychic, maybe try again?");
-        // Then we'll call the reset. 
+        alert("Bruh, you're not really psychic, are you?");
         reset();
-    }
+    } 
 };
 
 
@@ -43,29 +70,3 @@ document.onkeyup = function(event) {
 
 
 
-
-
-/* PSEUDO/THOUGHTS
-(Need to set the total number of guesses per round to 9)
-
-
-
-You're going to make a game just like the one in the video. Essentially, the app randomly picks a letter, 
-and the user has to guess which letter the app chose. Put the following text on your page:
-
-Guess what letter I'm thinking of
-
-Wins: (# of times the user has guessed the letter correctly)
-
-Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-
-Guesses Left: (# of guesses left. This will update)
-
-Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
-
-*/
