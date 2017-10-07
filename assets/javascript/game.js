ba7ce66ba46f9611fbff02e4b0a07742507af908
nickoliasxii <nickoliasxii@gmail.com>
2017-10-06 23:15:47 -0500

    finished, brah

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
+// ARE YOU A PSYBRO?????
+
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
 var wins = 0;
 var losses = 0;
 var guesses = 12;
